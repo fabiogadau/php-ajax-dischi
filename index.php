c76560a8412c7fa8b2922da25ac0fb666e53b0bc
Fabio Gadau <fabiogadau92@gmail.com>
2020-05-20 16:10:07 +0200

    all ready for work

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>PHP AJAX Dischi</title>
+</head>
+<body>
+   
+   <!--
+      Stampare a schermo una decina di dischi musicali.
+      Utilizzare solo PHP per ora e non AJAX.
+      Stampare direttamente i dischi in pagina: al caricamento della pagina ci saranno tutti i dischi.
+      Utilizzare Html, Sass e PHP.
+   -->
+
+</body>
+</html>
