1491e941564fb4bdab5b3b0fb09ce6c41242cc93
Fabio Gadau <fabiogadau92@gmail.com>
2020-05-20 17:36:38 +0200

    php logic ok

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php // IMPORTS
+   include_once __DIR__ . '/partials/data/data.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,6 +17,18 @@
       Stampare direttamente i dischi in pagina: al caricamento della pagina ci saranno tutti i dischi.
       Utilizzare Html, Sass e PHP.
    -->
+
+   <main id="albums">
+      <?php
+         foreach ( $albums as $album ) { ?>
+            <div>
+               <img src="<?php echo $album['cover']; ?>" alt="cover">
+               <?php echo $album['album']; ?>
+               <?php echo $album['artist']; ?>
+               <?php echo $album['year']; ?>
+            </div>
+         <? } ?>
+   </main>
 
 </body>
 </html>
---
+++ partials/data/data.php
@@ -0,0 +1,93 @@
+<?php 
+   $albums = [
+      [
+         'cover' => 'http://localhost:80/Boolean/php-ajax-dischi/dist/img/area.jpg',
+         'album' => 'CRAC!',
+         'artist' => 'Area',
+         'year' => '1975'
+      ],
+      [
+         'cover' => 'http://localhost:80/Boolean/php-ajax-dischi/dist/img/bobmarley-wailers.jpg',
+         'album' => 'Babylon by Bus',
+         'artist' => 'Bob Marley & The Wailers',
+         'year' => '1978'
+      ],
+      [
+         'cover' => 'http://localhost:80/Boolean/php-ajax-dischi/dist/img/deep-purple.jpg',
+         'album' => 'Deep Purple in Concert',
+         'artist' => 'Deep Purple',
+         'year' => '1980'
+      ],
+      [
+         'cover' => 'http://localhost:80/Boolean/php-ajax-dischi/dist/img/dream-theater.jpg',
+         'album' => 'Metropolis Pt. 2: Scenes from a Memory',
+         'artist' => 'Dream Theater',
+         'year' => '1999'
+      ],
+      [
+         'cover' => 'http://localhost:80/Boolean/php-ajax-dischi/dist/img/foo-fighters.jpg',
+         'album' => 'Sonic Highways',
+         'artist' => 'Foo Fighters',
+         'year' => '2014'
+      ],
+      [
+         'cover' => 'http://localhost:80/Boolean/php-ajax-dischi/dist/img/linkin-park.jpg',
+         'album' => 'Meteora',
+         'artist' => 'Linkin Park',
+         'year' => '2003'
+      ],
+      [
+         'cover' => 'http://localhost:80/Boolean/php-ajax-dischi/dist/img/mastodon.jpg',
+         'album' => 'Emperor of Sand',
+         'artist' => 'Mastodon',
+         'year' => '2017'
+      ],
+      [
+         'cover' => 'http://localhost:80/Boolean/php-ajax-dischi/dist/img/mcr.jpg',
+         'album' => 'The Black Parade',
+         'artist' => 'My Chemical Romance',
+         'year' => '2006'
+      ],
+      [
+         'cover' => 'http://localhost:80/Boolean/php-ajax-dischi/dist/img/pink-floyd.jpg',
+         'album' => 'Atom Heart Mother',
+         'artist' => 'Pink Floyd',
+         'year' => '1970'
+      ],
+      [
+         'cover' => 'http://localhost:80/Boolean/php-ajax-dischi/dist/img/qotsa.jpg',
+         'album' => 'Songs for the Deaf',
+         'artist' => 'Queens Of The Stone Age',
+         'year' => '2002'
+      ],
+      [
+         'cover' => 'http://localhost:80/Boolean/php-ajax-dischi/dist/img/queen.jpg',
+         'album' => 'News of the World',
+         'artist' => 'Queen',
+         'year' => '1977'
+      ],
+      [
+         'cover' => 'http://localhost:80/Boolean/php-ajax-dischi/dist/img/radiohead.jpg',
+         'album' => 'OK Computer',
+         'artist' => 'Radiohead',
+         'year' => '1997'
+      ],
+      [
+         'cover' => 'http://localhost:80/Boolean/php-ajax-dischi/dist/img/snarky-puppy.jpg',
+         'album' => 'We Like It Here',
+         'artist' => 'Snarky Puppy',
+         'year' => '2014'
+      ],
+      [
+         'cover' => 'http://localhost:80/Boolean/php-ajax-dischi/dist/img/soundgarden.jpg',
+         'album' => 'Badmotorfinger',
+         'artist' => 'Soundgarden',
+         'year' => '1991'
+      ],
+      [
+         'cover' => 'http://localhost:80/Boolean/php-ajax-dischi/dist/img/stevie-wonder.jpg',
+         'album' => 'Songs in the Key of Life',
+         'artist' => 'Stevie Wonder',
+         'year' => '1976'
+      ],
+   ];
